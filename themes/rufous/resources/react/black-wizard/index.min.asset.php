7d69ed463fbfb77edccb18b1fe49efb2c51647b7
rkbalolkhan <rk.balolkhan@gmail.com>
2026-01-02 04:15:08 +0530

    hero slider added

---
+++ themes/rufous/resources/react/black-wizard/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'c1353a592253ba005aea');
